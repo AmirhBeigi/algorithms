7cfd811bf23c7184f05d29a575b0ad2e7a6809f1
EhsanAkbarzadeh <ehsanakbarzadehtehran021@gmail.com>
2022-11-18 10:19:22 +0330

    fix(tests): change tests messages

--- lib/reduce/reduce.test.ts
+++ lib/reduce/reduce.test.ts
@@ -2,13 +2,13 @@ import { reduce } from "./index";
 import { describe, it, expect } from "vitest";
 
 describe("filter", () => {
-  it("number of array", () => {
+  it("should add all the members of the array together and return the number 10", () => {
     expect(
       reduce([1, 2, 3, 4], (previousValue, currentValue) => previousValue + currentValue, 0)
     ).toEqual(10);
   });
 
-  it("array of object", () => {
+  it("should add the id of all members of the array together and add 5 and return the number 8.", () => {
     const array = [
       {
         id: 1,
